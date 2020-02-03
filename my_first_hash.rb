045ace18865cda9f3a4bf338d3b362441746aa31
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 22:03:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,8 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"Whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+  the_manifest = {"Whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  return the_manifest
 end
 
 def retrieval
